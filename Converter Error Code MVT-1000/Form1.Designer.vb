daf08ce999c1538b5fb719d1cf6fb7f916b84f59
rnd.07 <rnd.07@korosispecindo.com>
2023-03-09 21:23:27 +0700

    first commit~

---
+++ Converter Error Code MVT-1000/Form1.Designer.vb
@@ -0,0 +1,452 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.Label15 = New System.Windows.Forms.Label()
+        Me.BIT_1 = New System.Windows.Forms.TextBox()
+        Me.BIT_2 = New System.Windows.Forms.TextBox()
+        Me.BIT_3 = New System.Windows.Forms.TextBox()
+        Me.BIT_4 = New System.Windows.Forms.TextBox()
+        Me.BIT_5 = New System.Windows.Forms.TextBox()
+        Me.BIT_6 = New System.Windows.Forms.TextBox()
+        Me.BIT_7 = New System.Windows.Forms.TextBox()
+        Me.BIT_8 = New System.Windows.Forms.TextBox()
+        Me.BIT_15 = New System.Windows.Forms.TextBox()
+        Me.BIT_14 = New System.Windows.Forms.TextBox()
+        Me.BIT_13 = New System.Windows.Forms.TextBox()
+        Me.BIT_12 = New System.Windows.Forms.TextBox()
+        Me.BIT_11 = New System.Windows.Forms.TextBox()
+        Me.BIT_10 = New System.Windows.Forms.TextBox()
+        Me.BIT_9 = New System.Windows.Forms.TextBox()
+        Me.START_CONVERTER = New System.Windows.Forms.Button()
+        Me.INPUT = New System.Windows.Forms.TextBox()
+        Me.Label16 = New System.Windows.Forms.Label()
+        Me.OUTPUT = New System.Windows.Forms.TextBox()
+        Me.Label17 = New System.Windows.Forms.Label()
+        Me.BIT_16 = New System.Windows.Forms.TextBox()
+        Me.Label18 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(54, 125)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(128, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "PARAMETER OUTPUT 1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(54, 168)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(128, 15)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "PARAMETER OUTPUT 2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(54, 216)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(128, 15)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "PARAMETER OUTPUT 3"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(54, 257)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(128, 15)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "PARAMETER OUTPUT 4"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(54, 300)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(128, 15)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "PARAMETER OUTPUT 5"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(55, 345)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(128, 15)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "PARAMETER OUTPUT 6"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(55, 389)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(128, 15)
+        Me.Label7.TabIndex = 6
+        Me.Label7.Text = "PARAMETER OUTPUT 7"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(54, 433)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(128, 15)
+        Me.Label8.TabIndex = 7
+        Me.Label8.Text = "PARAMETER OUTPUT 8"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(644, 125)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(128, 15)
+        Me.Label9.TabIndex = 8
+        Me.Label9.Text = "PARAMETER OUTPUT 9"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(644, 168)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(134, 15)
+        Me.Label10.TabIndex = 9
+        Me.Label10.Text = "PARAMETER OUTPUT 10"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(644, 216)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(134, 15)
+        Me.Label11.TabIndex = 10
+        Me.Label11.Text = "PARAMETER OUTPUT 11"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(644, 257)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(134, 15)
+        Me.Label12.TabIndex = 11
+        Me.Label12.Text = "PARAMETER OUTPUT 12"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(644, 300)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(134, 15)
+        Me.Label13.TabIndex = 12
+        Me.Label13.Text = "PARAMETER OUTPUT 13"
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Location = New System.Drawing.Point(644, 345)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(134, 15)
+        Me.Label14.TabIndex = 13
+        Me.Label14.Text = "PARAMETER OUTPUT 14"
+        '
+        'Label15
+        '
+        Me.Label15.AutoSize = True
+        Me.Label15.Location = New System.Drawing.Point(644, 389)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(134, 15)
+        Me.Label15.TabIndex = 14
+        Me.Label15.Text = "PARAMETER OUTPUT 15"
+        '
+        'BIT_1
+        '
+        Me.BIT_1.Location = New System.Drawing.Point(206, 122)
+        Me.BIT_1.Name = "BIT_1"
+        Me.BIT_1.Size = New System.Drawing.Size(322, 23)
+        Me.BIT_1.TabIndex = 15
+        '
+        'BIT_2
+        '
+        Me.BIT_2.Location = New System.Drawing.Point(206, 165)
+        Me.BIT_2.Name = "BIT_2"
+        Me.BIT_2.Size = New System.Drawing.Size(322, 23)
+        Me.BIT_2.TabIndex = 16
+        '
+        'BIT_3
+        '
+        Me.BIT_3.Location = New System.Drawing.Point(206, 208)
+        Me.BIT_3.Name = "BIT_3"
+        Me.BIT_3.Size = New System.Drawing.Size(322, 23)
+        Me.BIT_3.TabIndex = 17
+        '
+        'BIT_4
+        '
+        Me.BIT_4.Location = New System.Drawing.Point(206, 254)
+        Me.BIT_4.Name = "BIT_4"
+        Me.BIT_4.Size = New System.Drawing.Size(322, 23)
+        Me.BIT_4.TabIndex = 18
+        '
+        'BIT_5
+        '
+        Me.BIT_5.Location = New System.Drawing.Point(206, 297)
+        Me.BIT_5.Name = "BIT_5"
+        Me.BIT_5.Size = New System.Drawing.Size(322, 23)
+        Me.BIT_5.TabIndex = 19
+        '
+        'BIT_6
+        '
+        Me.BIT_6.Location = New System.Drawing.Point(206, 342)
+        Me.BIT_6.Name = "BIT_6"
+        Me.BIT_6.Size = New System.Drawing.Size(322, 23)
+        Me.BIT_6.TabIndex = 20
+        '
+        'BIT_7
+        '
+        Me.BIT_7.Location = New System.Drawing.Point(206, 386)
+        Me.BIT_7.Name = "BIT_7"
+        Me.BIT_7.Size = New System.Drawing.Size(322, 23)
+        Me.BIT_7.TabIndex = 21
+        '
+        'BIT_8
+        '
+        Me.BIT_8.Location = New System.Drawing.Point(206, 430)
+        Me.BIT_8.Name = "BIT_8"
+        Me.BIT_8.Size = New System.Drawing.Size(322, 23)
+        Me.BIT_8.TabIndex = 22
+        '
+        'BIT_15
+        '
+        Me.BIT_15.Location = New System.Drawing.Point(792, 386)
+        Me.BIT_15.Name = "BIT_15"
+        Me.BIT_15.Size = New System.Drawing.Size(322, 23)
+        Me.BIT_15.TabIndex = 29
+        '
+        'BIT_14
+        '
+        Me.BIT_14.Location = New System.Drawing.Point(792, 342)
+        Me.BIT_14.Name = "BIT_14"
+        Me.BIT_14.Size = New System.Drawing.Size(322, 23)
+        Me.BIT_14.TabIndex = 28
+        '
+        'BIT_13
+        '
+        Me.BIT_13.Location = New System.Drawing.Point(792, 297)
+        Me.BIT_13.Name = "BIT_13"
+        Me.BIT_13.Size = New System.Drawing.Size(322, 23)
+        Me.BIT_13.TabIndex = 27
+        '
+        'BIT_12
+        '
+        Me.BIT_12.Location = New System.Drawing.Point(792, 254)
+        Me.BIT_12.Name = "BIT_12"
+        Me.BIT_12.Size = New System.Drawing.Size(322, 23)
+        Me.BIT_12.TabIndex = 26
+        '
+        'BIT_11
+        '
+        Me.BIT_11.Location = New System.Drawing.Point(792, 208)
+        Me.BIT_11.Name = "BIT_11"
+        Me.BIT_11.Size = New System.Drawing.Size(322, 23)
+        Me.BIT_11.TabIndex = 25
+        '
+        'BIT_10
+        '
+        Me.BIT_10.Location = New System.Drawing.Point(792, 165)
+        Me.BIT_10.Name = "BIT_10"
+        Me.BIT_10.Size = New System.Drawing.Size(322, 23)
+        Me.BIT_10.TabIndex = 24
+        '
+        'BIT_9
+        '
+        Me.BIT_9.Location = New System.Drawing.Point(792, 122)
+        Me.BIT_9.Name = "BIT_9"
+        Me.BIT_9.Size = New System.Drawing.Size(322, 23)
+        Me.BIT_9.TabIndex = 23
+        '
+        'START_CONVERTER
+        '
+        Me.START_CONVERTER.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.START_CONVERTER.Location = New System.Drawing.Point(427, 37)
+        Me.START_CONVERTER.Name = "START_CONVERTER"
+        Me.START_CONVERTER.Size = New System.Drawing.Size(101, 35)
+        Me.START_CONVERTER.TabIndex = 30
+        Me.START_CONVERTER.Text = "CONVERT"
+        Me.START_CONVERTER.UseVisualStyleBackColor = True
+        '
+        'INPUT
+        '
+        Me.INPUT.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.INPUT.Location = New System.Drawing.Point(236, 37)
+        Me.INPUT.Name = "INPUT"
+        Me.INPUT.Size = New System.Drawing.Size(172, 35)
+        Me.INPUT.TabIndex = 31
+        '
+        'Label16
+        '
+        Me.Label16.AutoSize = True
+        Me.Label16.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label16.Location = New System.Drawing.Point(52, 44)
+        Me.Label16.Name = "Label16"
+        Me.Label16.Size = New System.Drawing.Size(165, 21)
+        Me.Label16.TabIndex = 32
+        Me.Label16.Text = "INPUT ERROR CODE "
+        '
+        'OUTPUT
+        '
+        Me.OUTPUT.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.OUTPUT.Location = New System.Drawing.Point(792, 37)
+        Me.OUTPUT.Name = "OUTPUT"
+        Me.OUTPUT.Size = New System.Drawing.Size(322, 35)
+        Me.OUTPUT.TabIndex = 33
+        '
+        'Label17
+        '
+        Me.Label17.AutoSize = True
+        Me.Label17.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label17.Location = New System.Drawing.Point(644, 44)
+        Me.Label17.Name = "Label17"
+        Me.Label17.Size = New System.Drawing.Size(102, 21)
+        Me.Label17.TabIndex = 34
+        Me.Label17.Text = "BIT OUTPUT"
+        '
+        'BIT_16
+        '
+        Me.BIT_16.Location = New System.Drawing.Point(792, 430)
+        Me.BIT_16.Name = "BIT_16"
+        Me.BIT_16.Size = New System.Drawing.Size(322, 23)
+        Me.BIT_16.TabIndex = 36
+        '
+        'Label18
+        '
+        Me.Label18.AutoSize = True
+        Me.Label18.Location = New System.Drawing.Point(644, 433)
+        Me.Label18.Name = "Label18"
+        Me.Label18.Size = New System.Drawing.Size(134, 15)
+        Me.Label18.TabIndex = 35
+        Me.Label18.Text = "PARAMETER OUTPUT 16"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1180, 521)
+        Me.Controls.Add(Me.BIT_16)
+        Me.Controls.Add(Me.Label18)
+        Me.Controls.Add(Me.Label17)
+        Me.Controls.Add(Me.OUTPUT)
+        Me.Controls.Add(Me.Label16)
+        Me.Controls.Add(Me.INPUT)
+        Me.Controls.Add(Me.START_CONVERTER)
+        Me.Controls.Add(Me.BIT_15)
+        Me.Controls.Add(Me.BIT_14)
+        Me.Controls.Add(Me.BIT_13)
+        Me.Controls.Add(Me.BIT_12)
+        Me.Controls.Add(Me.BIT_11)
+        Me.Controls.Add(Me.BIT_10)
+        Me.Controls.Add(Me.BIT_9)
+        Me.Controls.Add(Me.BIT_8)
+        Me.Controls.Add(Me.BIT_7)
+        Me.Controls.Add(Me.BIT_6)
+        Me.Controls.Add(Me.BIT_5)
+        Me.Controls.Add(Me.BIT_4)
+        Me.Controls.Add(Me.BIT_3)
+        Me.Controls.Add(Me.BIT_2)
+        Me.Controls.Add(Me.BIT_1)
+        Me.Controls.Add(Me.Label15)
+        Me.Controls.Add(Me.Label14)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "PARAMETER OUTPUT 16 BIT"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label14 As Label
+    Friend WithEvents Label15 As Label
+    Friend WithEvents BIT_1 As TextBox
+    Friend WithEvents BIT_2 As TextBox
+    Friend WithEvents BIT_3 As TextBox
+    Friend WithEvents BIT_4 As TextBox
+    Friend WithEvents BIT_5 As TextBox
+    Friend WithEvents BIT_6 As TextBox
+    Friend WithEvents BIT_7 As TextBox
+    Friend WithEvents BIT_8 As TextBox
+    Friend WithEvents BIT_15 As TextBox
+    Friend WithEvents BIT_14 As TextBox
+    Friend WithEvents BIT_13 As TextBox
+    Friend WithEvents BIT_12 As TextBox
+    Friend WithEvents BIT_11 As TextBox
+    Friend WithEvents BIT_10 As TextBox
+    Friend WithEvents BIT_9 As TextBox
+    Friend WithEvents START_CONVERTER As Button
+    Friend WithEvents INPUT As TextBox
+    Friend WithEvents Label16 As Label
+    Friend WithEvents OUTPUT As TextBox
+    Friend WithEvents Label17 As Label
+    Friend WithEvents BIT_16 As TextBox
+    Friend WithEvents Label18 As Label
+End Class
---
+++ Converter Error Code MVT-1000/Form1.vb
@@ -0,0 +1,85 @@
+﻿
+'============================================================================
+' Name        : Form1.vb
+' Author      : Abdurrahman Nurhakim
+' Version     : 1.0
+' Copyright   : Your copyright notice
+' Description : Decimal Error Code to Binary 16-bit
+'============================================================================
+Imports System.Runtime.InteropServices.JavaScript.JSType
+
+Public Class Form1
+
+    Dim InputDecimal
+    Dim Ouput_LSB_String
+
+    Public Function SelectStatus(Input_String As Char, Tag_name As String) As String
+        Dim Output As String
+        If Input_String = "1" Then
+            Output = "ERROR !!!, Please Check " + Tag_name
+        Else
+            Output = "STATUS OKE"
+        End If
+        Return Output
+    End Function
+
+    Public Function DecimalToBinaryLSB(ByVal decimalValue As Integer) As String
+        Dim binaryValue As String = ""
+        Dim bitValue As Integer
+
+        While decimalValue > 0
+            bitValue = decimalValue Mod 2
+            binaryValue = bitValue.ToString() & binaryValue
+            decimalValue = decimalValue \ 2
+        End While
+
+        ' Pad the binary value with zeros to make it 16 bits long
+        Do While Len(binaryValue) < 16
+            binaryValue = "0" & binaryValue
+        Loop
+
+        ' Reverse the binary value to get the LSB format
+        binaryValue = StrReverse(binaryValue)
+
+        ' Return the binary value as a string
+        DecimalToBinaryLSB = binaryValue
+        Return DecimalToBinaryLSB
+    End Function
+
+    Private Sub TextBox10_TextChanged(sender As Object, e As EventArgs) Handles BIT_15.TextChanged
+
+    End Sub
+
+    Private Sub START_CONVERTER_Click(sender As Object, e As EventArgs) Handles START_CONVERTER.Click
+        InputDecimal = CInt(INPUT.Text) 'convert String to Decimal (Integer)
+        Ouput_LSB_String = DecimalToBinaryLSB(InputDecimal)
+        OUTPUT.Text = Ouput_LSB_String
+        OUTPUT.Text = StrReverse(OUTPUT.Text)
+        Dim myCharArray() As Char = Ouput_LSB_String.ToCharArray()
+
+        BIT_1.Text = SelectStatus(myCharArray(0), Label1.Text)
+        BIT_2.Text = SelectStatus(myCharArray(1), Label2.Text)
+        BIT_3.Text = SelectStatus(myCharArray(2), Label3.Text)
+        BIT_4.Text = SelectStatus(myCharArray(3), Label4.Text)
+        BIT_5.Text = SelectStatus(myCharArray(4), Label5.Text)
+        BIT_6.Text = SelectStatus(myCharArray(5), Label6.Text)
+        BIT_7.Text = SelectStatus(myCharArray(6), Label7.Text)
+        BIT_8.Text = SelectStatus(myCharArray(7), Label8.Text)
+        BIT_9.Text = SelectStatus(myCharArray(8), Label9.Text)
+        BIT_10.Text = SelectStatus(myCharArray(9), Label10.Text)
+        BIT_11.Text = SelectStatus(myCharArray(10), Label11.Text)
+        BIT_12.Text = SelectStatus(myCharArray(11), Label12.Text)
+        BIT_13.Text = SelectStatus(myCharArray(12), Label13.Text)
+        BIT_14.Text = SelectStatus(myCharArray(13), Label14.Text)
+        BIT_15.Text = SelectStatus(myCharArray(14), Label15.Text)
+        BIT_16.Text = SelectStatus(myCharArray(15), Label18.Text)
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+End Class
